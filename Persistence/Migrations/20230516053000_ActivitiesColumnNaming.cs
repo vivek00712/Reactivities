e5ef767ee74fef3289c6e63437de598d919531e6
Vivek Patel <vivek00712@gmail.com>
2023-05-17 09:31:21 +0530

    [Task]: Creation of Skeleton of .NET Core

---
+++ Persistence/Migrations/20230516053000_ActivitiesColumnNaming.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Persistence.Migrations
+{
+    /// <inheritdoc />
+    public partial class ActivitiesColumnNaming : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.RenameColumn(
+                name: "Categogy",
+                table: "Activities",
+                newName: "Category");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.RenameColumn(
+                name: "Category",
+                table: "Activities",
+                newName: "Categogy");
+        }
+    }
+}
