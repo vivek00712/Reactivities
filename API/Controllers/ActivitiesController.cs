6a12a0f18bbd1e9afca42d8fa4ad79882cac4e85
Vivek Patel <vivek00712@gmail.com>
2023-06-13 10:59:47 +0530

    [Tasks] : [Activities' CRUD Operations using CQRS]

--- API/Controllers/ActivitiesController.cs
+++ API/Controllers/ActivitiesController.cs
@@ -54,7 +54,20 @@ namespace API.Controllers
         [HttpGet("{id}")] //api/activities/id
         public async Task<ActionResult<Activity>> GetActivity(Guid id)
         {
-            return Ok();
+            return await Mediator.Send(new Details.Query { Id = id });
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> CreateActivity(Activity activity)
+        {
+            return Ok(await Mediator.Send(new Create.Command { Activity = activity }));
+        }
+
+        [HttpPut("{id}")]
+        public async Task<IActionResult> EditActivitiy(Guid id, Activity activity)
+        {
+            activity.Id = id;
+            return Ok(await Mediator.Send(new Edit.Command { Activity = activity }));
         }
     }
 }
---
+++ Application/Activities/Edit.cs
@@ -0,0 +1,36 @@
+using MediatR;
+using Domain;
+using Persistence;
+using Microsoft.EntityFrameworkCore;
+
+namespace Application.Activities
+{
+    public class Edit
+    {
+        public class Command : IRequest
+        {
+            public Activity Activity { get; set; }
+        }
+
+        public class Handler : IRequestHandler<Command>
+        {
+            private readonly DataContext _context;
+            public Handler(DataContext context)
+            {
+                _context = context;
+            }
+
+            public async Task<Unit> Handle(Command request, CancellationToken cancellationToken)
+            {
+                var activity = await _context.Activities.FindAsync(request.Activity.Id);
+
+                activity.Title = request.Activity.Title ?? request.Activity.Title;
+
+                await _context.SaveChangesAsync();
+
+                return Unit.Value;
+            }
+        }
+
+    }
+}
