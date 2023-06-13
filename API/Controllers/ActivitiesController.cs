490cb938e2f3f2dcc2a28569bfabe141350c57ef
Vivek Patel <vivek00712@gmail.com>
2023-06-13 07:25:38 +0530

    [Tasks] : [Implementation of MediatR]

--- API/Controllers/ActivitiesController.cs
+++ API/Controllers/ActivitiesController.cs
@@ -2,28 +2,49 @@ using Domain;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.EntityFrameworkCore;
 using Persistence;
+using MediatR;
+using Application.Activities;
 
 namespace API.Controllers
 {
     public class ActivitiesController : BaseApiController
     {
-        private readonly DataContext _context;
+        private readonly IMediator _mediator;
 
-        public ActivitiesController(DataContext context)
+        // private readonly DataContext _context;
+
+        // public ActivitiesController(DataContext context)
+        // {
+        //     _context = context;
+        // }
+
+        // [HttpGet] //localhost/api/activities
+        // public async Task<ActionResult<List<Activity>>> GetActivities()
+        // {
+        //     return await _context.Activities.ToListAsync();
+        // }
+
+        // [HttpGet("{id}")] //api/activities/id
+        // public async Task<ActionResult<Activity>> GetActivity(Guid id)
+        // {
+        //     return await _context.Activities.FindAsync(id);
+        // }
+
+        public ActivitiesController(IMediator mediator)
         {
-            _context = context;
+            this._mediator = mediator;
         }
 
-        [HttpGet] //localhost/api/activities
+        [HttpGet] //api/activities
         public async Task<ActionResult<List<Activity>>> GetActivities()
         {
-            return await _context.Activities.ToListAsync();
+            return await _mediator.Send(new List.Query());
         }
 
         [HttpGet("{id}")] //api/activities/id
         public async Task<ActionResult<Activity>> GetActivity(Guid id)
         {
-            return await _context.Activities.FindAsync(id);
+            return Ok();
         }
     }
 }
